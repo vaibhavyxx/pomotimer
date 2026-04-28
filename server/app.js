f6932a002671d7386db4454d338f04de520fa26e
v <vd5443@rit.edu>
2026-04-28 18:33:01 -0400

    imported the favicon

--- server/app.js
+++ server/app.js
@@ -31,7 +31,7 @@ redisClient.connect().then(() => {
 
     app.use(helmet());
     app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
-    app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+    app.use(favicon(`${__dirname}/../hosted/img/favicon/favicon.png`));
     app.use(compression());
     app.use(express.urlencoded({extended: true}));
     app.use(express.json());
